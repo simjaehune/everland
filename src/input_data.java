96af0104e7a3b5965c14da24043f9a762228c8c2
simjaehune <86578389+simjaehune@users.noreply.github.com>
2021-07-01 23:02:02 +0900

    Create input_data.java

---
+++ src/input_data.java
@@ -0,0 +1,22 @@
+package myever;
+
+public class input_data {
+	
+	int register_number=0;
+	int count=0; 
+	int previlege=0;
+	int price=0;
+	int ticket=0;
+	
+	
+	public input_data(int ticket, int register_number, int count ,int price, int previlege) {
+		
+		this.ticket = ticket;
+		this.register_number=register_number;
+		this.previlege=previlege;
+		this.count=count;
+		this.price = price;
+		
+	}
+
+}
